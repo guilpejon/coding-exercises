6d5e3bb31e254bc089249cee3ce90c1e94cb2b8d
Guilherme Ferratti Pejon <guilpejon@gmail.com>
2022-02-19 17:31:43 -0300

    Updated template

--- TEMPLATE.rb
+++ TEMPLATE.rb
@@ -1,34 +1,40 @@
 ##### DESCRIPTION #####
 ##### INPUT #####
 string1 = 
+string2 =
 output1 = 
-string2 = 
+string3 = 
+string4 = 
 output2 = 
 
 # Time complexity: T(n) =
 # Space complexity: S(n) =
-def first_solution(string)
+def first_solution(string1, string2)
 end
 
 # Time complexity: T(n) =
 # Space complexity: S(n) =
-def second_solution(string)
+# HASH
+def second_solution(string1, string2)
 end
 
 # Time complexity: T(n) =
 # Space complexity: S(n) =
-def third_solution(string)
+def third_solution(string1, string2)
 end
 
+puts "###############################"
+puts "INPUT: ['#{string1}','#{string2}']"
+puts "OUTPUT: #{output1}"
+puts "###############################"
+puts "1st: #{first_solution(string1, string2)} (#{(output1 == first_solution(string1, string2)) ? "RIGHT" : "WRONG"})"
+puts "2nd: #{second_solution(string1, string2)} (#{(output1 == second_solution(string1, string2)) ? "RIGHT" : "WRONG"})"
+puts "3rd: #{third_solution(string1, string2)} (#{(output1 == third_solution(string1, string2)) ? "RIGHT" : "WRONG"})"
 puts
-puts "FIRST SOLUTION"
-puts "IN: '#{string1}'. OUT: #{output1}. SOLUTION: #{first_solution(string1)}"
-puts "IN: '#{string2}'. OUT: #{output2}. SOLUTION: #{first_solution(string2)}"
-puts
-puts "SECOND SOLUTION"
-puts "IN: '#{string1}'. OUT: #{output1}. SOLUTION: #{second_solution(string1)}"
-puts "IN: '#{string2}'. OUT: #{output2}. SOLUTION: #{second_solution(string2)}"
-puts
-puts "THIRD SOLUTION"
-puts "IN: '#{string1}'. OUT: #{output1}. SOLUTION: #{third_solution(string1)}"
-puts "IN: '#{string2}'. OUT: #{output2}. SOLUTION: #{third_solution(string2)}"
+puts "###############################"
+puts "INPUT: ['#{string3}','#{string4}']"
+puts "OUTPUT: #{output2}"
+puts "###############################"
+puts "1st: #{first_solution(string3, string4)} (#{(output2 == first_solution(string3, string4)) ? "RIGHT" : "WRONG"})"
+puts "2nd: #{second_solution(string3, string4)} (#{(output2 == second_solution(string3, string4)) ? "RIGHT" : "WRONG"})"
+puts "3rd: #{third_solution(string3, string4)} (#{(output2 == third_solution(string3, string4)) ? "RIGHT" : "WRONG"})"
