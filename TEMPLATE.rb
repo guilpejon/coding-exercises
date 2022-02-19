7c6ce6d3002d1354a5e6ed9e4aec4c7dbc4c0003
Guilherme Ferratti Pejon <guilpejon@gmail.com>
2022-02-19 16:41:32 -0300

    Updated template

--- TEMPLATE.rb
+++ TEMPLATE.rb
@@ -1,10 +1,34 @@
 ##### DESCRIPTION #####
 ##### INPUT #####
-##### OUTPUT #####
+string1 = 
+output1 = 
+string2 = 
+output2 = 
 
 # Time complexity: T(n) =
 # Space complexity: S(n) =
-def first_solution()
+def first_solution(string)
 end
 
-puts "First solution: #{first_solution()}"
+# Time complexity: T(n) =
+# Space complexity: S(n) =
+def second_solution(string)
+end
+
+# Time complexity: T(n) =
+# Space complexity: S(n) =
+def third_solution(string)
+end
+
+puts
+puts "FIRST SOLUTION"
+puts "IN: '#{string1}'. OUT: #{output1}. SOLUTION: #{first_solution(string1)}"
+puts "IN: '#{string2}'. OUT: #{output2}. SOLUTION: #{first_solution(string2)}"
+puts
+puts "SECOND SOLUTION"
+puts "IN: '#{string1}'. OUT: #{output1}. SOLUTION: #{second_solution(string1)}"
+puts "IN: '#{string2}'. OUT: #{output2}. SOLUTION: #{second_solution(string2)}"
+puts
+puts "THIRD SOLUTION"
+puts "IN: '#{string1}'. OUT: #{output1}. SOLUTION: #{third_solution(string1)}"
+puts "IN: '#{string2}'. OUT: #{output2}. SOLUTION: #{third_solution(string2)}"
