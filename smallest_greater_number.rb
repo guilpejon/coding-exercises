f53223bfa2b19f428db08bd45c9485b95072d391
Guilherme Ferratti Pejon <guilpejon@gmail.com>
2022-02-28 00:32:12 -0300

    Added smallest greater number exercise

---
+++ smallest_greater_number.rb
@@ -0,0 +1,27 @@
+# Given sorted array, find smallest greater number after x
+
+def lower_bound(arr, x)
+  left = 0
+  right = arr.size - 1
+
+  return left if arr[left] >= x
+  return -1 if arr[right] < x
+
+  while (left + 1 < right) # arr[left] < x && arr[right] >= x
+    mid = (left + right) / 2
+    if arr[mid] < x
+      left = mid
+    else
+      right = mid
+    end
+  end
+
+  return right
+end
+
+[0 1 2 3 4 5 6 ]
+[1 3 5 6 7 8 10]
+
+x = 7
+left = 5
+right = 7
